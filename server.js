2ca4f1f726d0253729b50206ab7daec861578d61
David Poore <dpoore@pivotal.io>
2016-08-26 15:07:45 -0600

    initial commit for hack day project - analyze story acceptance by day

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + "/c3-0.4.11"));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
