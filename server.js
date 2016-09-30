e3234c1f237494d6bac9957eadc73a6f0531d452
David Poore <dpoore@pivotal.io>
2016-09-30 11:18:34 -0600

    add basic styling and a loading screen

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(__dirname + "/c3-0.4.11"));
+app.use(express.static(__dirname + "/css"));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
